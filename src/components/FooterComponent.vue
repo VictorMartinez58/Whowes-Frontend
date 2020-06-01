21cedc1f409be36a0951116e41b94a0faa1964c7
VictorMartinez58 <victor.vax.58@gmail.com>
2020-06-01 18:26:41 +0200

    first login

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer id="footer">
+            <div class="center">
+                <p>
+                    &copy; Hey im a footer waiting for copyright 
+                </p>
+            </div>
+        </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterComponent'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,42 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+import Vuelidate from 'vuelidate';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
+import VueSwal from 'vue-swal';
+
+
+import Login from './components/auth/login.vue';
+import Register from './components/auth/register.vue';
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+Vue.use(Vuelidate);
+Vue.use(VueSwal);
+
+const routes = [
+  
+  {path: '/login', component: Login},
+  {path: '/register', component: Register},
+
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+  base: process.env.BASE_URL,
+});
+
+
+const base = axios.create({
+  baseURL: "http://localhost:5858"
+});
+Vue.prototype.$http = base;
+
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
