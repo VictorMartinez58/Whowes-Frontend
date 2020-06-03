9c40f04de76f778f10b00e2b9a9a2f1015699969
VictorMartinez58 <victor.vax.58@gmail.com>
2020-06-03 15:43:32 +0200

    routerfile

--- src/main.js
+++ src/main.js
@@ -1,61 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 import Vuelidate from 'vuelidate';
+import VueSwal from 'vue-swal';
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.css";
-import VueSwal from 'vue-swal';
 
-
-import Login from './components/auth/login.vue';
-import Register from './components/auth/register.vue';
-import Home from './components/Home.vue';
-import UserDetails from './components/user/UserDetails.vue';
-import ErrorComponent from './components/ErrorComponent.vue';
 
 Vue.config.productionTip = false
 
-Vue.use(VueRouter);
 Vue.use(Vuelidate);
 Vue.use(VueSwal);
-
-const routes = [
-  
-  {path: '/', component: Home},
-  {path: '/login', component: Login},
-  {path: '/register', component: Register},
-  {path: '/home', component: Home,  meta: {
-    requiresAuth: true
-  }},
-  {path: '/user-details', component: UserDetails,  meta: {
-    requiresAuth: true
-  }},
-  {path: '*', component: ErrorComponent},
-
-
-];
-
-const router = new VueRouter({
-  routes,
-  mode: 'history',
- 
-});
-
-router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (localStorage.getItem("jwt") == null) {
-      next({
-        path: "/login"
-      });
-    } else {
-      next();
-    }
-  } else {
-    next();
-  }
-});
-
-
 
 const base = axios.create({
   baseURL: "http://localhost:5858"
---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from "vue";
+import VueRouter from 'vue-router';
+
+
+import Login from '../components/auth/login.vue';
+import Register from '../components/auth/register.vue';
+import Home from '../components/Home.vue';
+import UserDetails from '../components/user/UserDetails.vue';
+import ErrorComponent from '../components/ErrorComponent.vue';
+
+
+Vue.use(VueRouter);
+
+
+
+const routes = [
+  
+    {path: '/', component: Home,  meta: {
+      requiresAuth: true
+    }},
+    {path: '/login', component: Login},
+    {path: '/register', component: Register},
+    {path: '/home', component: Home,  meta: {
+      requiresAuth: true
+    }},
+    {path: '/user-details', component: UserDetails,  meta: {
+      requiresAuth: true
+    }},
+    {path: '*', component: ErrorComponent},
+  
+  
+  ];
+  
+  const router = new VueRouter({
+    routes,
+    mode: 'history',
+   
+  });
+  
+  router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+      if (localStorage.getItem("jwt") == null) {
+        next({
+          path: "/login"
+        });
+      } else {
+        next();
+      }
+    } else {
+      next();
+    }
+  });
+
+export default router;
